15c94e3def24bada527e518741f854d195a7c944
gmaneschy <145074809+gmaneschy@users.noreply.github.com>
2026-06-16 12:10:38 -0300

    base

---
+++ backend/apps/feed/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# apps/feed/views.py
+# from apps.itineraries.serializers import ItinerarioSerializer
+# feed reaproveita serializer do itinerário em vez de ter o seu próprio
+
+# Create your views here.
